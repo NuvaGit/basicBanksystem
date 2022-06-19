c629ac9bea95be97a742f39c53dd661512672fc9
Jack Neilan <102314039+NuvaGit@users.noreply.github.com>
2022-06-19 19:22:58 +0100

    Add files via upload
    the user class i use to store details of bank members

---
+++ User.java
@@ -0,0 +1,15 @@
+package monoply;
+import java.util.Scanner;
+
+public class User {
+String name;
+String password;
+int pass;
+int cash = 2000;
+Scanner input = new Scanner(System.in);
+
+
+}
+
+
+
