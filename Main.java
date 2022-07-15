4dc9c84896d3052ee67cf18ff82c57fe9606672a
NUVA <102314039+NuvaGit@users.noreply.github.com>
2022-07-15 18:15:35 +0100

    reworked

--- Main.java
+++ Main.java
@@ -8,7 +8,31 @@ public class Main {
 	
 public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
+	String shibe = 
+		      "                Y.                      _             \n" +
+		      "                YiL                   .```.           \n" +    
+		      "   much world   Yii;                .; .;;`.          \n" +
+		      "                YY;ii._           .;`.;;;; :          \n" +
+		      " such hello     iiYYYYYYiiiii;;;;i` ;;::;;;;          \n" +
+		      "            _.;YYYYYYiiiiiiYYYii  .;;.   ;;;          \n" +
+		      "         .YYYYYYYYYYiiYYYYYYYYYYYYii;`  ;;;;          \n" +
+		      "       .YYYYYYY$$YYiiYY$$$$iiiYYYYYY;.ii;`..          \n" +
+		      "      :YYY$!.  TYiiYY$$$$$YYYYYYYiiYYYYiYYii.         \n" +
+		      "      Y$MM$:   :YYYYYY$! `` 4YYYYYiiiYYYYiiYY.        \n" +
+		      "   `. :MM$$b.,dYY$$Yii  :'   :YYYYllYiiYYYiYY         \n" +
+		      "_.._ :`4MM$!YYYYYYYYYii,.__.diii$$YYYYYYYYYYY         \n" +
+		      ".,._ $b`P`      4$$$$$iiiiiiii$$$$YY$$$$$$YiY;        \n" +
+		      "   `,.`$:       :$$$$$$$$$YYYYY$$$$$$$$$YYiiYYL       \n" +
+		      "     `;$$.    .;PPb$`.,.``T$$YY$$$$YYYYYYiiiYYU:      \n" +
+		      "    ;$P$;;: ;;;;i$y$ !Y$$$b;$$$Y$YY$$YYYiiiYYiYY      \n" +
+		      "    $Fi$$ .. ``:iii.`- :YYYYY$$YY$$$$$YYYiiYiYYY      \n" +
+		      "    :Y$$rb ````  `_..;;i;YYY$YY$$$$$$$YYYYYYYiYY:     \n" +
+		      "     :$$$$$i;;iiiiidYYYYYYYYYY$$$$$$YYYYYYYiiYYYY.    \n" +
+		      "      `$$$$$$$YYYYYYYYYYYYY$$$$$$YYYYYYYYiiiYYYYYY    \n" +
+		      "      .i!$$$$$$YYYYYYYYY$$$$$$YYY$$YYiiiiiiYYYYYYY    \n" +
+		      "     :YYiii$$$$$$$YYYYYYY$$$$YY$$$$YYiiiiiYYYYYYi'    ";
 
+   System.out.println(shibe);
 	int currentuser=0;
 	
     System.out.println("please enter the number of users");
